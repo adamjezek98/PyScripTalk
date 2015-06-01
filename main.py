5f54b32b70fc6c4a54a487ecb193229e46a3b350
adam <jezek.adamek@gmail.com>
2015-06-01 16:03:43 +0200

    added messagecount

--- main.py
+++ main.py
@@ -13,17 +13,18 @@ class PyScripTalk():
         
         self.db = MySQLdb.connect(self.DatabaseAdress, self.Username, self.Password, \
                              self.DatabaseName)
+        self.cursor = self.db.cursor()
 
         
 
     def test(self):
-        self.cursor = self.db.cursor()
         self.cursor.execute("SELECT * FROM %s " % self.TabName)
         return self.cursor.fetchall()
 
     def addMessage(self, messagetext, auth):
         try:
-            self.cursor.execute("INSERT INTO %s (content, auth) VALUES ('%s', '%s') " % (self.TabName, messagetext, auth))
+            self.cursor.execute("INSERT INTO %s (content, auth) VALUES ('%s', '%s') " % \
+                                (self.TabName, messagetext, auth))
             self.db.commit()
             return True
         except:
@@ -36,8 +37,21 @@ class PyScripTalk():
         data["content"] = result[1]
         data["auth"] = result[2]
         return data
+
+    def getMessages(self, count=1):
+        self.cursor.execute("SELECT * FROM %s ORDER BY 'ID' LIMIT %s " % (self.TabName, count))
+        result = self.cursor.fetchall()
+        data = []
+        for line in result:
+            data.append({"content":line[1],"auth":line[2]})
+        return data
+
+    def getMessagesCount(self):
+        self.cursor.execute("SELECT COUNT(*) FROM %s" % self.TabName)
+        result = self.cursor.fetchone()
+        return result[0]
+        
         
 pst = PyScripTalk()
 pst.init()
-print pst.test()
-print pst.getFirtsMessage()
+print pst.getMessages(5)
